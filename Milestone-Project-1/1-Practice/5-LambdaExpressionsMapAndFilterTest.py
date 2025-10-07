8e4697d75aec20ea83a9883f8d28bfe46e1baa53
kundan Singh <thegurukulsiksha@gmail.com>
2025-10-07 07:45:24 +0530

    Adding Warm Up Project

---
+++ Milestone-Project-1/1-Practice/5-LambdaExpressionsMapAndFilterTest.py
@@ -0,0 +1,105 @@
+"""
+_____________________________________________________________________________________________
+
+ Lambda Expressions, Map, and Filter
+_____________________________________________________________________________________________
+
+General Question: What Is lambda And where we can Use it?
+
+General Answer:
+_____________________________________________________________________________________________
+
+Question1: Write a lambda function to add 10 to a number.
+Answer1:
+"""
+
+#___________________________________________________________________________
+#Question2: Write a lambda function to multiply two numbers.
+
+#Answer2:
+
+#___________________________________________________________________________
+# Question3: Sort a list of tuples by the second value using lambda.
+# pairs = [(1, 3), (2, 2), (5, 1)]
+#Output should be  [(5, 1), (2, 2), (1, 3)]
+
+# Answer3:
+
+
+#___________________________________________________________________________
+# Question4:  Use map() to square all numbers in a list.
+
+# Answer4:
+
+#___________________________________________________________________________
+# #Question5: Convert a list of strings to uppercase. words = ["hello", "python", "lambda"]
+
+#Answer5:
+
+#___________________________________________________________________________
+#Question6: Add two lists element-wise using map(). a = [1, 2, 3], b = [4, 5, 6] output should be
+# [5, 7, 9]
+
+#Answer6:
+
+#___________________________________________________________________________
+#Question7: Filter even numbers from a list. nums = [1, 2, 3, 4, 5, 6]
+
+#Answer7:
+
+
+#___________________________________________________________________________
+#Question8: Filter names starting with "A". names = ["Alice", "Bob", "Amanda", "Charlie"]
+#Using 2 Methods
+
+#Answer8:
+#Method1:
+
+#Method2:
+
+#___________________________________________________________________________
+#Question9: Filter numbers greater than 50. numbers = [10, 55, 32, 100, 75]
+
+#Answer9:
+
+#___________________________________________________________________________
+#Question10: Use map() and filter() to get squares of even numbers only. nums = [1, 2, 3, 4, 5, 6]
+
+#Answer10:
+
+#___________________________________________________________________________
+#Question11: Extract palindromes from a list of words. words = ["madam", "python", "level", "world"]
+
+#Answer11:
+
+
+#___________________________________________________________________________
+#Question12: Sort a list of dictionaries by "age" using lambda.
+#people = [{"name": "Alice", "age": 25}, {"name": "Bob", "age": 20}, {"name": "Charlie", "age": 30}]
+
+#Answer12:
+
+#___________________________________________________________________________
+#Question13: Use map() to get the length of each string. words = ["Python", "Lambda", "Filter", "Map"]
+
+#Answer13:
+
+#___________________________________________________________________________
+#Question14: Combine map() and filter() to get cube of odd numbers only. nums = [1, 2, 3, 4, 5, 6]
+
+#Answer14:
+
+
+#___________________________________________________________________________
+#Question15: Given a list of transactions, filter out failed ones and return only the amounts
+# squared.
+# transactions = [
+#     {"id": 1, "amount": 100, "status": "success"},
+#     {"id": 2, "amount": 200, "status": "failed"},
+#     {"id": 3, "amount": 300, "status": "success"},
+# ]
+
+#Answer15:
+
+#___________________________________________________________________________
+
---
+++ Milestone-Project-1/2-Warm Up Project/PickAndPlace.py
@@ -0,0 +1,77 @@
+"""
+_____________________________________________________________________________________________
+
+It is time to get you to put together all your skills to start building usable projects!
+Before you jump into our full milestone project, we will go through some warm-up component
+exercises, to get you comfortable with a few key ideas we use in the milestone projectand
+larger projects in general, specifically:
+------------------
+Getting User Input
+Creating Functions that edit variables based on user input
+Generating output
+Joining User Inputs and Logic Flow
+_____________________________________________________________________________________________
+
+"""
+#Create a small game where a user can choose a "position" in an existing list and replace it with
+#a value of their choice.
+
+# Example:
+
+# PS C:\Users\v-kundsingh\Projects\AI\Python\Milestone-Project-1\Warm Up Project> python .\Solution.py
+# Here is the current list
+# [0, 1, 2]
+
+# Pick a position to replace (0,1,2): 0
+# Type a string to place at the position10
+# Here is the current list
+# ['10', 1, 2]
+
+# Would you like to keep playing? Y or N Y
+# Here is the current list
+# ['10', 1, 2]
+
+# Pick a position to replace (0,1,2): 2
+# Type a string to place at the position5
+# Here is the current list
+# ['10', 1, '5']
+
+# Would you like to keep playing? Y or N N
+
+def display_list(list1):
+    print("Here is the Current List:")
+    print(list1)
+
+def choice_position():
+    choice = ''
+    while choice not in ['0','1','2']:
+        choice = input("Pick a position where you want to replace(0, 1, 2): ")
+        if choice not in ['0','1','2']:
+            print("Sorry! Worng Choice!! Please enter from (0, 1, 2).")
+    return int(choice)
+
+def replace_choice(list1,choice):
+    user_input = input("Please enter the string to replace: ")
+    list1[choice] = user_input
+    return list1
+
+def gameon_choice():
+    choice = ''
+    while choice not in ['Y','N']:
+        choice = input("Would You like to keep playing? Y or N: ")
+        if choice not in ['Y','N']:
+            print("Wrong Input. Please select Y or N.")
+
+    if choice == 'Y':
+        return True
+    else:
+        return False
+
+game_on = True
+list1 = [0,0,0]
+while game_on:
+    display_list(list1)
+    choice = choice_position()
+    list1 = replace_choice(list1,choice)
+    display_list(list1)
+    game_on = gameon_choice()
