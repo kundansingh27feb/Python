f7e6f8771042d400f5f8dc51957a6b3959fe57f7
kundan Singh <kundansingh27feb@gmail.com>
2025-10-24 11:27:16 +0530

    added python advance concept OOPS.
    * Adding Python Basic Tests
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Warm Up Project
    
    * Adding Opps Concept
    
    ---------
    
    Co-authored-by: kundan Singh <thegurukulsiksha@gmail.com>
    Co-authored-by: Kundan Singh (HCL Technologies Corporate Services) <v-kundsingh@microsoft.com>

---
+++ Milestone-Project-2/1-PracticeTest/Practice-solutions/1-Oops-Homework-Solution.py
@@ -0,0 +1,21 @@
+class Line(object):
+
+    def __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    
+    def slope(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return (y2-y1)/(x2-x1)
+    
+coor1 = (3,2)
+coor2 = (8,9)
+li = Line(coor1,coor2)
+print("Distance:", li.distance())
+print("Slope:", li.slope())
